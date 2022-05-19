83c5e9deff946410c171bcf5dd878f72791221eb
EvelynGMaldonado <evelyn.gmaldonado@gmail.com>
2022-05-19 16:39:56 -0700

    working on displaying random meal

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+const meals = document.getElementById("meals");
+
 getRandomMeal();
 
 async function getRandomMeal() {
@@ -7,7 +9,7 @@ async function getRandomMeal() {
     const randomMeal = respRanData.meals[0];
     console.log(randomMeal);
 
-    loadRandomMeal();
+    addMeal(randomMeal, true);
 }
 
 async function getMealById(id) {
@@ -16,4 +18,32 @@ async function getMealById(id) {
 }
 async function getMealBySearch(name) {
     const mealByName = await fetch("https://www.themealdb.com/api/json/v1/1/search.php?s=" + name);
+}
+
+function addMeal(mealData, random = false) {
+    const meal = document.createElement("div");
+    meal.classList.add("meal");
+    meal.innerHTML = `
+        <div class="meal-header">
+            ${
+                random ? `
+            <span class="random"> Random Recipe </span> ` 
+            : "" 
+            }
+            <img src="${mealData.strMealThumb}" alt="${mealData.strMeal}">
+        </div>
+        <div class="meal-body">
+            <h4>${mealData.strMeal}</h4>
+            <button class="fav-btn">
+                <image class="fav-btn active" src="./assets/suit-heart-fill.svg"></image>
+            </button>
+        </div>
+    `;
+
+    //event listener to mark favorite meals by clicking the herart button
+    meal.querySelector(".meal-body .fav-btn").addEventListener("click", () => {
+        alert(`${mealData.strMeal} is now part of your favorite meals`);
+    });
+
+    meals.appendChild(meal);
 }
